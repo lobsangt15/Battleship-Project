6ebb5fe4971394f1d0e7e8a30399257ea9d6a347
BT_1N3_08 <efikwefqwerhioefefhfqwefwefhiowefhio@fwefhiohioefhioefhiowefhiowefhiowefhioefhioefhwefhioefhiowef.edu>
2025-03-19 11:00:22 -0400

    printjing boartd

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -5,5 +5,23 @@ public class GameBoard {
     private Player player;
     private Scanner scanner;
 
+    private void setupBoard() {
+        board = new Space[8][8];
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[r].length; c++) {
+                if (board[r][c] == null) {
+                    board[r][c] = new Space("O");
+                }
+            }
+        }
+    }
+    private void printBoard() {
+        for (Space[] row : board) {
+            for (Space element : row) {
+                System.out.print(element.getSymbol());
+            }
+            System.out.println();
+        }
+    }
 
 }
