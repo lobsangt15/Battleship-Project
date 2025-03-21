0a716b73f5aca3ae30aecd299558bc96a675c92d
BT_1N3_08 <guyeruhefwuhwefuhfefwefwefwefuhwefhiowefhiwefwefuwefuhwefwefwefuhwefewuhwefwefuhwefuhweweuuhwefwefwefuwefwefwefwefuhwe@>
2025-03-21 10:49:24 -0400

    chase makes half of ai class

--- src/AI.java
+++ src/AI.java
@@ -8,19 +8,23 @@ public class AI {
         this.Random = Random;
     }
     public int makeMoves() {
-        int x;
-        int y;
+        int x = 0;
+        int y = 0;
         boolean hit = false;
-
+        int turns = 1;
+        boolean AITURN = false;
 //plan : if the hit hits battleship, set to true and give another turn, to make ai smart either + 1 the column or -1 or +1 row
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
-                if (hit) {
-                    System.out.println("The enemy's battleship has been hit.");
-                    board[r][c] = /
-                }
+                x = (int) Math.floor(Math.random() * 11);
+                y = (int) Math.floor(Math.random() * 11);
+            }
+            if (board[x][y] = hit) {
+                turns++;
+
             }
         }
+        return x;
     }
 
 }
