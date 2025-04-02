0eb33bc549e805974bf748146541c35848f2e505
BT_1N3_06 <tenzin.hardcarrying@hardcarrier.com>
2025-04-02 12:42:38 -0400

    log off

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         GameBoard board = new GameBoard();
         board.setupBoard();
+        System.out.println("i deleted all your code :)");
     }
 }
