277fd31f8795bef046060744ead9bd2ada5d8dc3
BT_1N3_06 <tenzin.hardcarrying@hardcarrier.com>
2025-04-03 10:12:29 -0400

    Tenzin (removed stuff made by someone messing around with code)

--- src/main.java
+++ src/main.java
@@ -2,6 +2,5 @@ public class main {
     public static void main(String[] args) {
         GameBoard board = new GameBoard();
         board.setupBoard();
-        System.out.println("i deleted all your code :)");
     }
 }
