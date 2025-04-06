366a965d90d866e35c3a0ee7765920c2b665ff32
Ten547 <tenzink62@nycstudents.net>
2025-04-05 22:43:12 -0400

    Tenzin (worked on space)

--- src/Space.java
+++ src/Space.java
@@ -1,11 +1,27 @@
 public class Space {
     private String symbol;
+    private boolean isHit;
 
     public Space(String symbol, int shipPartsNum) {
         this.symbol = symbol;
+        this.isHit = false;
     }
 
     public String getSymbol() {
         return symbol;
+    }
+
+    public boolean isHit() {
+        return isHit;
+    }
+
+    public void markAsHit() {
+        isHit = true;
+        symbol = "💥";
+    }
+
+    public void markAsMiss() {
+        isHit = true;
+        symbol = "❌";
     }
 }
