b661dcf76b9b4bcc96e8d0307da00755fd0230ba
Ten547 <tenzink62@nycstudents.net>
2025-04-05 22:50:14 -0400

    Tenzin (player class now keeps track of items)

--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,9 @@ public class Player {
     private int points;
     private int score;
     private int moves;
+    private int numBombs = 0;
+    private int numScoutPlanes = 0;
+    private int numRecallPanels = 0;
 
     public Player(String name, int points, int score, int moves) {
         this.name = name;
@@ -30,4 +33,57 @@ public class Player {
     public void usePoints(int amount) {
         points -= amount;
     }
+
+    public void addPoints(int amount) {
+        points += amount;
+    }
+
+    public void addBombs(int amount) {
+        numBombs += amount;
+    }
+
+    public boolean useBomb() {
+        if (numBombs > 0) {
+            numBombs--;
+            return true;
+        }
+        return false;
+    }
+
+    public void addScoutPlanes(int amount) {
+        numScoutPlanes += amount;
+    }
+
+    public boolean useScoutPlane() {
+        if (numScoutPlanes > 0) {
+            numScoutPlanes--;
+            return true;
+        }
+        return false;
+    }
+
+    public void addRecallPanels(int amount) {
+        numRecallPanels += amount;
+    }
+
+    public boolean useRecallPanel() {
+        if (numRecallPanels > 0) {
+            numRecallPanels--;
+            return true;
+        }
+        return false;
+    }
+
+    public int getBombs() {
+        return numBombs;
+    }
+
+    public int getScoutPlanes() {
+        return numScoutPlanes;
+    }
+
+    public int getRecallPanels() {
+        return numRecallPanels;
+    }
+
 }
