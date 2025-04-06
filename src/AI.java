95361fe074b2a87daeaf51b077d8044f39ef5a2f
Ten547 <tenzink62@nycstudents.net>
2025-04-05 22:44:07 -0400

    Tenzin (worked on AI)

--- src/AI.java
+++ src/AI.java
@@ -1,67 +1,44 @@
-import java.util.Scanner;
-
 public class AI {
-    private Space[][] AIboard;
-    private Player player;
-    private Scanner scanner;
+    private Space[][] board;
 
-    public AI () {
-        scanner = new Scanner(System.in);
-        setupAIBoard();
-        play();
+    public AI() {
+        board = new Space[10][10];
+        setupBoard();
     }
 
-    public void setupAIBoard() {
-        AIboard = new Space[10][10];
-        for (int r = 0; r < AIboard.length; r++) {
-            for (int c = 0; c < AIboard[r].length; c++) {
-                if (AIboard[r][c] == null) {
-                    AIboard[r][c] = new Space("☐", 0);
+    public void setupBoard() {
+        for (int r = 0; r < 10; r++) {
+            for (int c = 0; c < 10; c++) {
+                board[r][c] = new Space("☐", 0);
+            }
+        }
+
+        board[2][2] = new Battleship("⛴", 4);
+        board[5][5] = new AircraftCarrier("🛳", 6, true);
+        board[7][7] = new UnmannedShip("⛵", 2, true);
+    }
+
+    public Space[][] getBoard() {
+        return board;
+    }
+
+    public void takeTurn(Space[][] playerBoard) {
+        boolean turnOngoing = true;
+        while (turnOngoing) {
+            int x = (int)(Math.random() * 10);
+            int y = (int)(Math.random() * 10);
+            Space target = playerBoard[x][y];
+
+            if (!target.isHit()) {
+                if (target instanceof Battleship || target instanceof AircraftCarrier || target instanceof UnmannedShip) {
+                    System.out.println("AI hit your ship at (" + x + ", " + y + ")!");
+                    target.markAsHit();
+                } else {
+                    System.out.println("AI missed at (" + x + ", " + y + ").");
+                    target.markAsMiss();
+                    turnOngoing = false;
                 }
             }
         }
-    }
-
-    public void printBoard() {
-        for (Space[] row : AIboard) {
-            for (Space element : row) {
-                System.out.print(element.getSymbol());
-            }
-            System.out.println();
-        }
-    }
-
-    private void play() {
-        boolean gamePlaying = true;
-        player = new Player("Player", 0, 0, 0);
-
-        System.out.println("Get ready, " + player.getName() + ". The game is starting!");
-        printBoard();
-
-        while (gamePlaying) {
-            System.out.println("Enter which row you'd like to attack (0-9): ");
-            int row = scanner.nextInt();
-            System.out.println("Enter which column you'd like to attack (0-9): ");
-            int column = scanner.nextInt();
-
-            if (row < 0 || row > 9  || column < 0  || column > 9) {
-                System.out.println("Invalid coordinates.");
-                continue;
-            }
-
-            Space target = AIboard[row][column];
-
-            if (target instanceof Battleship || target instanceof AircraftCarrier || target instanceof UnmannedShip) {
-                System.out.println("Hit!");
-            } else {
-                System.out.println("Miss!");
-            }
-
-            printBoard();
-
-            gamePlaying = false;
-        }
-
-        System.out.println("Game Over!");
     }
 }
